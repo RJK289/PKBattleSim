167dc11d2bbe97467d4ac977463e5659f2924173
RJK289 <abrkoller@gmail.com>
2025-02-12 14:52:23 -0500

    Battle!

--- main.py
+++ main.py
@@ -26,15 +26,10 @@ class Monster:
         self.hp = max(self.hp - damage, 0)
 
     def choose_move(self):
-     
         return random.choice(self.moves)
 
 def take_turn(attacker, defender):
-  
-    # The attacker chooses a move
     chosen_move = attacker.choose_move()
-
-    # Check if the move hits
     hit_chance = random.random()
     if hit_chance <= chosen_move.accuracy:
         damage = chosen_move.calculate_damage(attacker, defender)
@@ -43,12 +38,10 @@ def take_turn(attacker, defender):
         print(f"It dealt {damage} damage to {defender.name}!")
     else:
         print(f"{attacker.name} tried to use {chosen_move.name}, but it missed!")
-
     if defender.is_fainted():
         print(f"{defender.name} has fainted!")
 
 def battle(monster1, monster2):
-    
     print(f"A wild battle has begun between {monster1.name} and {monster2.name}!")
     
     while not monster1.is_fainted() and not monster2.is_fainted():
@@ -70,3 +63,32 @@ def battle(monster1, monster2):
         print(f"{monster1.name} fainted. {monster2.name} is the winner!")
     else:
         print(f"{monster2.name} fainted. {monster1.name} is the winner!")  
+
+def main():
+    # Moves
+    tackle = Move(name="Tackle", power=40, accuracy=0.95)
+    water_gun = Move(name="Water Gun", power=40, accuracy=1.0)
+
+    # Monsters
+    creature = Monster(
+        name="Creature1",
+        hp=45,
+        attack=49,
+        defense=49,
+        speed=45,
+        moves=[tackle]
+    )
+
+    creature2 = Monster(
+        name="Creature2",
+        hp=44,
+        attack=48,
+        defense=65,
+        speed=43,
+        moves=[tackle, water_gun]
+    )
+
+    battle(creature, creature2)
+
+if __name__ == "__main__":
+    main()
